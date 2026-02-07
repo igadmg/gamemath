a254d9863710a6742ea62d86eebe7357777c8248
Igor Karatayev <igor.cwer@gmail.com>
2026-02-08 00:36:56 +0300

    added some doc comments

--- vector2/vector2.iter.go
+++ vector2/vector2.iter.go
@@ -23,7 +23,9 @@ func (v Vector[T]) EnumRegion(w, h T, step ...T) iter.Seq[Vector[T]] {
 	}
 }
 
-func (v Vector[T]) EnumRegionAround(w, h T, step ...T) iter.Seq[Vector[T]] {
+// Enumerate coordinates around vector position
+// rw and rh are width and height radiuses of enumerated region
+func (v Vector[T]) EnumRegionAround(wr, hr T, step ...T) iter.Seq[Vector[T]] {
 	return func(yield func(Vector[T]) bool) {
 		sx := T(1)
 		sy := T(1)
@@ -34,8 +36,8 @@ func (v Vector[T]) EnumRegionAround(w, h T, step ...T) iter.Seq[Vector[T]] {
 			sy = step[1]
 		}
 
-		for y := v.Y - h; y <= v.Y+h; y += sy {
-			for x := v.X - w; x <= v.X+w; x += sx {
+		for y := v.Y - hr; y <= v.Y+hr; y += sy {
+			for x := v.X - wr; x <= v.X+wr; x += sx {
 				if !yield(Vector[T]{X: x, Y: y}) {
 					return
 				}
